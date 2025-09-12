e627a11fa0b6f783a954570eb513200ea4df6c20
Claudiu Dobre <137715041+Claudiu-DobreO@users.noreply.github.com>
2025-09-12 12:03:46 -0400

    implemented youtube http api

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,18 +2,19 @@ import Head from "next/head";
 import Navbar from '@/components/nav/navbar';
 import Banner from "@/components/banner/banner";
 import SectionCards from "@/components/card/section-cards";
-import { getVideos } from "@/lib/videos";
+import { getVideos, getPopularVideos } from "@/lib/videos";
 import styles from "@/styles/Home.module.css";
 
 export const getServerSideProps = async () => {
   const disneyVideos = await getVideos('disney cartoon trailers');
   const travelVideos = await getVideos('travel');
   const productivityVideos = await getVideos('productivity');
-  // const popularVideos = await getVideos('disney cartoon trailers');
-  return { props: { disneyVideos, travelVideos, productivityVideos }};
+  const popularVideos = await getPopularVideos('US');
+
+  return { props: { disneyVideos, travelVideos, productivityVideos, popularVideos }};
 }
 
-export default function Home({ disneyVideos, travelVideos, productivityVideos }) {
+export default function Home({ disneyVideos, travelVideos, productivityVideos, popularVideos }) {
   return (
     <>
       <Head>
@@ -22,6 +23,7 @@ export default function Home({ disneyVideos, travelVideos, productivityVideos })
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <div
         className={`${styles.page}`}
       > 
@@ -37,7 +39,7 @@ export default function Home({ disneyVideos, travelVideos, productivityVideos })
           <SectionCards title='Disney' videos={disneyVideos} size='large' />
           <SectionCards title='Travel' videos={travelVideos} size='small' />
           <SectionCards title='Productivity' videos={productivityVideos} size='medium' />
-          <SectionCards title='Popular' videos={disneyVideos} size='small' />
+          <SectionCards title='Popular' videos={popularVideos} size='small' />
         </div>
         
       </div>
