1ed7ba93446bf6bae8ae642bfcc39fadd1a3fdd7
Claudiu Dobre <137715041+Claudiu-DobreO@users.noreply.github.com>
2025-09-20 06:27:04 -0400

    updated to deny access to home page if user is not logged in and redirect to login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { magic } from "@/lib/magic-client";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const protect = async () => {
+      try {
+        if (!magic) return; // SSR
+
+        const isLoggedIn = await magic.user.isLoggedIn();
+        console.log({isLoggedIn});
+
+        if (isLoggedIn) {
+          router.push('/');
+        } else { 
+          router.push('/login');
+        }
+
+      } catch (error) {
+        console.error('Somethign went wrong logging in: ', error);
+        router.push('/login');
+      }
+    }
+
+    protect();
+  }, []);
+
+  useEffect(() => {
+      const handleComplete = () => setIsLoading(false);
+
+      router.events.on('routeChangeComplete', handleComplete);
+      router.events.on('routeChangeError', handleComplete);
+      
+      return () => {
+          router.events.off('routeChangeComplete', handleComplete);
+          router.events.off('routeChangeError', handleComplete);
+      }
+  }, [router]);
+
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />;
 }
